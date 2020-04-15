28245d4162a8bbf27e04e12c44efc2e19fd9a2fd
Goodness Ifejesu Olajide <ifejesugoodness97@gmail.com>
2020-04-15 13:02:00 +0100

    feat: integrate xml response to the api

--- routes/estimator.js
+++ routes/estimator.js
@@ -1,19 +1,8 @@
 const express = require('express');
 const fs = require('fs');
-
+const o2x = require('object-to-xml');
 const router = express.Router();
 const estimator = require('../src/estimator');
-
-const checkParameter = (res, parameter) => {
-  if (parameter === 'json') {
-    return res.setHeader('Content-Type', 'application/json');
-  }
-  if (parameter === 'xml') {
-    return res.setHeader('Content-Type', 'application/xml');
-  }
-
-  throw new Error('Check your url again');
-};
 
 const getEstimate = (req) => {
   const inputData = {
@@ -32,31 +21,33 @@ const getEstimate = (req) => {
 
   return estimator(inputData);
 };
+const checkParameter = (res, parameter) => {
+  if (parameter === 'json') {
+    return res.setHeader('Content-Type', 'application/json');
+  }
+  if (parameter === 'xml') {
+    return res.setHeader('Content-Type', 'application/xml');
+  }
+
+  throw new Error('Check your url again');
+};
 
 router.post('/', (req, res) => {
   const result = getEstimate(req);
-  const { data, impact, severeImpact } = result;
-
-  res.status(200).json({
-    data,
-    impact,
-    severeImpact
-  });
+  res.status(200).json(result);
 });
 
-router.post('/:optional', (req, res) => {
-  const urlParam = req.params.optional;
-
+router.post('/:format', (req, res) => {
+  const urlParam = req.params.format;
   checkParameter(res, urlParam);
-
   const result = getEstimate(req);
-  const { data, impact, severeImpact } = result;
 
-  res.status(200).json({
-    data,
-    impact,
-    severeImpact
-  });
+  if (urlParam === 'json') {
+    res.status(200).json(result);
+  }
+  if (urlParam === 'xml') {
+    res.status(200).send(o2x(result));
+  }
 });
 
 router.get('/logs', (req, res) => {
